207d8f25515574d41b3346ff9402538c037774a4
Joe <joej21@gmail.com>
2017-02-14 21:25:24 -0500

    adding views and forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,31 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
+	before_action :return_user, only: [:edit, :update]
 
 	def index
 		@users = User.all
-		print "------------"
-		print "index action working"
-		print "------------"
 	end
 
 	def edit
-		print "------------"
-		print "edit action working"
-		print "------------"
 	end
 
 	def update
-		print "------------"
-		print "update action working"
-		print "------------"
+		if @user.update(user_params)
+			redirect_to :root
+		else
+			render :action => :edit
+		end
+	end
+
+	# --------------------------------------------------------
+	private
+
+	def return_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :fav_game)
 	end
 
 end
