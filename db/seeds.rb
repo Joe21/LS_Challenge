ea68ed7f035c332d5e0b3a667cf50b9fda2f18fa
Joe <joej21@gmail.com>
2017-02-16 18:04:12 -0500

    adding new seed file to pass pw complexity validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 User.delete_all
 
-User.create! email: "batman@gmail.com", password: "password", password_confirmation: "password", created_at: Time.now
-User.create! email: "superman@gmail.com", password: "password", password_confirmation: "password", created_at: Time.now - 1.day
-User.create! email: "wonderwoman@gmail.com", password: "password", password_confirmation: "password", created_at: Time.now - 1.day
-User.create! email: "greenlantern@gmail.com", password: "password", password_confirmation: "password", created_at: Time.now - 1.day
-User.create! email: "flash@gmail.com", password: "password", password_confirmation: "password", created_at: Time.now - 4.day
-User.create! email: "cyborg@gmail.com", password: "password", password_confirmation: "password", created_at: Time.now - 5.day
-User.create! email: "martianmanhunter@gmail.com", password: "password", password_confirmation: "password", created_at: Time.now - 5.day
+User.create! email: "batman@gmail.com", password: "Password!1", password_confirmation: "Password!1", created_at: Time.now
+User.create! email: "superman@gmail.com", password: "Password!1", password_confirmation: "Password!1", created_at: Time.now - 1.day
+User.create! email: "wonderwoman@gmail.com", password: "Password!1", password_confirmation: "Password!1", created_at: Time.now - 1.day
+User.create! email: "greenlantern@gmail.com", password: "Password!1", password_confirmation: "Password!1", created_at: Time.now - 1.day
+User.create! email: "flash@gmail.com", password: "Password!1", password_confirmation: "Password!1", created_at: Time.now - 4.day
+User.create! email: "cyborg@gmail.com", password: "Password!1", password_confirmation: "Password!1", created_at: Time.now - 5.day
+User.create! email: "martianmanhunter@gmail.com", password: "Password!1", password_confirmation: "Password!1", created_at: Time.now - 5.day
