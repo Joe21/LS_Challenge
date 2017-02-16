b61660cc24f0933753f71b099aec1427ebd34c78
Joe <joej21@gmail.com>
2017-02-16 00:12:03 -0500

    adding minor comments for reasability to chartscontroller

--- app/controllers/api/v1/charts_controller.rb
+++ app/controllers/api/v1/charts_controller.rb
@@ -1,14 +1,9 @@
 module Api::V1
 	class ChartsController < ApiController
-		require 'pp'
+		# require 'pp'
 
 		# GET /v1/sign_up_chart
 		def sign_up_chart
-			# query = User.order(created_at: :desc)
-			# puts "------------------------------------------------------------"
-			# pp query.first.created_at.strftime('%x')
-			# puts "------------------------------------------------------------"
-
 			data = {}
 			User.order(created_at: :asc).each do |user|
 				if data[user.created_at.strftime('%x')]
@@ -18,15 +13,10 @@ module Api::V1
 				end
 			end
 
-			puts "=========================================="
-			pp data
-			puts "=========================================="
-		
 			respond_to do |format|
 				format.json { render :json => data }
 			end
-		end
+		end # <---- end of sign_up_chart action
 
-
-	end
+	end # <---- end of ChartsController class
 end
