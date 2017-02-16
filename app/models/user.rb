1dc9d9655e2f80234885a1125c95f3f0c06c1f5c
Joe <joej21@gmail.com>
2017-02-16 17:59:55 -0500

    adding password complexity, requires 8-40 characters, 20-7E ascii character, 1 digit, 1 uppercase, and 1 lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validate :password_complexity
+  def password_complexity
+  	if password.present?
+  		# --------------------------------------
+  		# RegEx Notes
+  		# --------------------------------------
+  		# Contain a digit => (?=.*\d)
+  		# Contain a lowercase => (?=.*([a-z]))
+  		# Contain a uppercase => (?=.*([A-Z]))
+  		# Contains a special character from 20-7E ascii value => ([\x20-\x7E])
+  		# Must be at least 8 and no longer than 40 characters
+			reg = /^(?=.*\d)(?=.*([a-z]))(?=.*([A-Z]))([\x20-\x7E]){8,40}$/
+			if !password.match(reg)
+				errors.add :password, "must include 1 uppercase, 1 lowercase, 1 digit, and 1 special character"
+			end
+  	end
+  end
+
 end
