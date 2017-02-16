d42a26959f8a78865ead06ac7871bf469b08a94f
Joe <joej21@gmail.com>
2017-02-16 17:28:05 -0500

    resized chart by disabling responsive, if responsive desired, encapsulate fixed positions inside canvas options

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -21,13 +21,15 @@ function parseChartData(data) {
 	var chartData = {
 		labels: [],
 		datasets: [{
-			label: "DATE",
+			label: "# Sign Ups",
 			data: [],
 			backgroundColor: ['rgb(211,211,211)'],
 			borderColor: ['rgb(169,169,169)'],
 			borderWidth: 1
 		}]
-	}	
+	}
+	// Fill chartData['labels'] array with each day (string format) 
+	// Fill chartData['datasets'][0]['data'] with the # of sign ups
 	for(key in data) {
 		chartData['labels'].push(key)
 		chartData['datasets'][0]['data'].push(data[key])
@@ -37,12 +39,13 @@ function parseChartData(data) {
 }
 
 function buildChart(chartData) {
-	var ctx = $('#signUpChart');
-
+	var ctx = document.getElementById('signUpChart');
 	var signUpChart = new Chart(ctx, {
 		type: 'bar',
 		data: chartData,
 		options: {
+			maintainAspectRatio: true,
+			responsive: false,
       scales: {
       	xAxes: [{
       		categorySpacing: 0
